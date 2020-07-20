321416e2a877a24699b2314326f8f20c267dc1d1
Ian Baker <iabaker@hotmail.com>
2020-07-20 12:11:21 -0500

    Add project files.

---
+++ TandemApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TandemApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        
+        [HttpGet]
+        public User Get(string emailAddress)
+        {
+
+            return new User
+            {
+                Name = "Test"
+            };
+
+        }
+    }
+}
---
+++ TandemApi/User.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TandemApi
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
